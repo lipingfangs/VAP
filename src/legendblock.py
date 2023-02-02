6427e895fa2a2d6ee1ed9e601531f006028a820b
pingping <46209789+lipingfangs@users.noreply.github.com>
2023-02-02 22:15:18 +0800

    Update legendblock.py

--- src/legendblock.py
+++ src/legendblock.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 def legendblock(colors,text,xpoi,ypoi,legendheight,coefficient,laynum):
     laynumcof = laynum/30
     left, bottom, width, height = (xpoi, ypoi,coefficient,  legendheight) 
-    legendblock=mpatches.Rectangle((left,bottom+laynumcof),width,height, 
+    legendblock=mpatches.Rectangle((left,bottom),width,height, 
                                         fill=True,
                                         color=colors,
                                        linewidth=2) 

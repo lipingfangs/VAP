f8c8e775bf841841d393eb8575f1dcd389c534f3
pingping <46209789+lipingfangs@users.noreply.github.com>
2023-02-02 10:49:20 +0800

    Create freqacq.py

---
+++ freqacq.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+frqfilename = sys.argv[1]
+pathwaybedfile = sys.argv[2]
+population = sys.argv[3]
+
+frqfile= open(frqfilename,"r")
+frqfilelist =   frqfile.readlines()
+frqfile.close()
+population = 
+dicfreq = {}
+for i in frqfilelist:
+    dicfreq[i.split()[1]] = {}
+    if i.split()[1] == "A":
+        dicfreq[i.split()[1]]["A"] = 1-float(i.split()[4])
+        dicfreq[i.split()[1]]["G"] = float(i.split()[4])
+    else:
+        dicfreq[i.split()[1]]["A"] = float(i.split()[4])
+        dicfreq[i.split()[1]]["G"] = 1- float(i.split()[4])
+    
+pathwaybedfile= open(pathwaybedfilename,"r")
+pathwaybedfilelist =   pathwaybedfile.readlines()
+pathwaybedfile.close()
+
+for i in pathwaybedfilelist:
+    pathchr = i.split()[0]
+    pathst = int(i.split()[1])
+    pathed = int(i.split()[2])
+    for j in dicfreq.keys():
+        freqchr = j.split("_")[0]
+        freqpoi  = int(j.split("_")[1])
+        if freqchr==pathchr and pathst < freqpoi < pathed:
+            print(freqpoi-50,freqpoi+50, dicfreq[j]["A"],populationmark)

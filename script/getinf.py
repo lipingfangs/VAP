a3be4b5c2a702dd16c0f6f22a9a0d218725b48b9
pingping <46209789+lipingfangs@users.noreply.github.com>
2023-02-08 16:42:00 +0800

    Update getinf.py

--- script/getinf.py
+++ script/getinf.py
@@ -2,8 +2,61 @@
 # -*- coding: utf-8 -*-
 # @name   : freqacq.py
 # @author : cat
-# @date   : 2023/2/2.
+# @date   : 2023/2/8.
+def locatetomsuornot(a,runlist, refchrlist):
+    
+    if a.split(":")[0] in refchrlist:
+        #print(a,end = ",")
+        #print("done")
+        vrdlist.append(a)
+        vrdlist.append(a)
+        return a
 
+    else:
+        achr= a.split(":")[0]
+        aloc = int(a.split(":")[1])
+        for i in runlist:
+            i = i.split()
+            inschr  = i[0].split("_")[0]
+            insst = int(i[0].split("_")[1])
+            insed = int(i[0].split("_")[2])
+            poshead =  i[1]
+            postail=  i[2]
+            posheadchr = poshead.split(":")[0]
+            postailchr = postail.split(":")[0]
+            if inschr ==  achr  and aloc>insst and aloc<insed:
+                vrdlist.append(i[0])
+                if posheadchr in refchrlist and  postailchr  in refchrlist:
+                    vrdlist.append(poshead)#,postail,end = ",")
+                    vrdlist.append(postail) #print("done")
+                    return a             
+                else:
+                    if posheadchr not in refchrlist and postailchr  in refchrlist:
+                        #print("tail",postail,end = ",")
+                        vrdlist.append(postail)
+                        a = locatetomsuornot(poshead,runlist, refchrlist)
+                    if postailchr not in refchrlist and posheadchr in refchrlist:
+                        vrdlist.append(poshead)
+                        #print("tail",poshead,end = ",")
+                        a = locatetomsuornot(postail,runlist, refchrlist)
+                    if postailchr not in refchrlist and posheadchr not in refchrlist:
+                        a = locatetomsuornot(poshead,runlist, refchrlist)
+                        a = locatetomsuornot(postail,runlist, refchrlist)
+
+                    
+
+def sortlocus(vrdlist,headerortail,refchrlist):  
+    sortpoi = []
+    #print(vrdlist)
+    for i in vrdlist[1:]:
+        if i.split(":")[0] in refchrlist:
+            sortpoi.append(i.split(":")[1])
+            chrn = i.split(":")[0]
+    if headerortail  == "head":       
+        return min(sortpoi), chrn
+    else:
+        return max(sortpoi), chrn
+    
 
 import sys
 goin = sys.argv[1]
@@ -23,7 +76,7 @@ dicstined ={}
 dicdirection = {}
 reflist= []
 genelistlnotonelevel = []
-
+genecleanlist =[]
 for i in genelistlorg:
     i = i.strip()
     if i.find(">") != -1:
@@ -34,8 +87,8 @@ for i in genelistlorg:
     
 
 for i in genelistl:
-     i = i.strip()
-     if len(i.split()) >2:
+    i = i.strip()
+    if len(i.split()) >2:
        # templine = ""
         #templine = "_".join(temp[0].split("_")[:-2])
         temp = i.strip().split()
@@ -47,6 +100,7 @@ for i in genelistl:
         else:
             dicdirection[temp[0][1:]] = "Forward"
         i = i.replace(">","").replace("<","")
+        genecleanlist.append(i)
        # print(i)
         temp = i.strip().split()
         if temp[1].split(":")[0] in reflist and temp[2].split(":")[0] in reflist:
@@ -71,56 +125,50 @@ for i in dicstin.keys():
     print(dicstin[i][0],end = ",")
     print(dicstin[i][1],end = ",")
     print(dicdirection[i])
+          
+#print("#onlyonelevel done")  
+          
+
+#print("Chr10Basmati1genomefa:130859",end=",")
+
 
 for i in genelistlnotonelevel:
-    
-    temp = i.strip().split()
-    temp[0] = temp[0]+","+temp[0].split("_")[-1]+","+temp[0].split("_")[-1]
-    posout = ""
-    posoneout = ""
-    postwoout = ""
-    #if temp[1].find("MSU") == -1 or temp[2].find("MSU") == -1:
-        #print(temp)
-    if temp[1].split(":")[0] not in reflist:
-        poshead = temp[1].split(":")[0]
-        posone = int(temp[1].split(":")[1])
-        for j in dicstin.keys():
-            jchr  = j.split("_")[0]
-            jst = int(j.split("_")[1])
-            jed = int(j.split("_")[2])
-            if jchr == poshead  and posone>jst and posone<jed:
-                if temp[2].split(":")[0] in reflist:
-                    posoneout += dicstin[j][0]  +","+  temp[2].split(":")[1]+","+dicstin[j][1]
-                else:
-                    posoneout += dicstin[j][0]  +","+dicstin[j][1]
-                    
+    c = 0
+    fourlist = ["","","","","",""]
+    #print(i)
+    for j in i.split()[1:]:
+        
+        #print(j)
+        c +=1
+        vrdlist = []
+        locatetomsuornot(j,genecleanlist,reflist)
+        if vrdlist == []:
+            donout ="yes"
+            break
+        else:
+            donout ="no"
             
-    if temp[2].split(":")[0] not in reflist:
-        poshead = temp[2].split(":")[0]
-        postwo = int(temp[2].split(":")[1])
-        for j in dicstin.keys():
-            jchr  = j.split("_")[0]
-            jst = int(j.split("_")[1])
-            jed = int(j.split("_")[2])
-            if jchr == poshead and postwo>jst and postwo<jed:
-                if temp[1].split(":")[0] in reflist:
-                    postwoout += temp[1].split(":")[1] + "," + dicstin[j][0]+","+dicstin[j][1]
-                else:
-                    postwoout += dicstin[j][0]+","+dicstin[j][1]
-
-    if postwoout == "" and    posoneout != "":
-        dicstined[temp[0]] = posoneout
-    elif postwoout != "" and    posoneout == "":
-        dicstined[temp[0]] = postwoout
-    elif postwoout != "" and    posoneout != "":
-        dicstined[temp[0]] = posoneout+","+postwoout
-        #break
-        #print(dicstined)
-#for i in dicstin.keys():
-    #print(i,end = ",")
-    #print(dicstin[i][0],end = ",")
-    #print(dicstin[i][1])               
-for i in    dicstined.keys():
-    print(i,end = ",")
-    print(dicstined[i],end = ",")
-    print(dicdirection[i])
+        if c ==1:
+            poi,chrn = sortlocus(vrdlist,"head",reflist)
+            directpro = vrdlist[0]
+            fourlist[0] = poi
+            fourlist[2] = chrn
+            fourlist[3] = "Forward"
+            fourlist[4] = j.replace(":",",")
+            
+        else:
+            poi,chrn = sortlocus(vrdlist,"tail",reflist)
+            directpro = vrdlist[0]
+            fourlist[1] = poi 
+            fourlist[5] = j.replace(":",",")
+            
+    clist = i.split()[0].split("_")
+    cl = 0
+    printlist = clist+fourlist
+    if donout != "yes":
+        for i in printlist:
+            cl +=1
+            if cl == len(printlist):
+                print(i)
+            else:
+                print(i,end = ",")
